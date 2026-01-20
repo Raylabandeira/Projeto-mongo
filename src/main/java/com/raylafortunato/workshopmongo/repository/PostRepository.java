ec1c5871a3027b2171eba794f040b6dd3cf85e67
Rayla <raylafortunato@gmail.com>
2026-01-19 21:30:46 -0300

    Consulta simples com query methods

--- src/main/java/com/raylafortunato/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/raylafortunato/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package com.raylafortunato.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.raylafortunato.workshopmongo.domain.Post;
 
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
